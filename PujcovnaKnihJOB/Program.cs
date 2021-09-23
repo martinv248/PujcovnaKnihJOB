4750f22654cb3485ed8621422a3d9815c811edf1
martinv248 <mvanek248@gmail.com>
2021-09-23 17:55:36 +0200

    Initial commit

---
+++ PujcovnaKnihJOB/Program.cs
@@ -0,0 +1,43 @@
+﻿using Hangfire;
+using Hangfire.Storage;
+using PujcovnaKnihJOB.Models;
+using PujcovnaKnihJOB.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PujcovnaKnihJOB
+{
+    class Program
+    {
+        private ModelServices svc = new ModelServices();
+
+        static void Main()
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage(@"Data Source=(localdb)\mssqllocaldb;Initial Catalog=master;Integrated Security=True");
+            using (var server = new BackgroundJobServer())
+            {
+                var p = new Program();
+                p.deleteJobs();
+                Console.WriteLine("Hangfire Server started. Press any key to close the application...");
+                RecurringJob.AddOrUpdate(() => p.svc.checkBooks(), Cron.Minutely);
+                Console.ReadKey();
+
+            }
+        }
+
+        private void deleteJobs()
+        {
+            using (var connection = JobStorage.Current.GetConnection())
+            {
+                foreach(var recurringJob in connection.GetRecurringJobs())
+                {
+                    RecurringJob.RemoveIfExists(recurringJob.Id);
+                }
+            }
+        }
+        
+    }
+}
---
+++ PujcovnaKnihJOB/Services/ModelServices.cs
@@ -0,0 +1,49 @@
+﻿using PujcovnaKnihJOB.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PujcovnaKnihJOB.Services
+{
+    class ModelServices
+    {
+        private dbEntities db = new dbEntities();
+
+        public void checkBooks()
+        {
+            var orders = from m in db.Orders select m;
+            var books = from n in db.Books select n;
+            string borrowState = "Žádost o zapůjčení";
+            string returnState = "Žádost o vrácení";
+
+            foreach(var order in orders)
+            {
+                if (order.State.Equals(borrowState))
+                {
+                    Books book = db.Books.Find(order.BookID);
+                    Console.WriteLine("Titul: " + book.Title + " Status: " + book.IsAvailable + " Order state: " + order.State);
+                    book.IsAvailable = "Půjčena";
+                    order.State = "Půjčena";
+                    order.BorrowDate = DateTime.Now;
+                    Console.WriteLine("Titul: " + book.Title + " Status: " + book.IsAvailable + " Order state: " + order.State + " Date: " + order.BorrowDate);
+                    db.Entry(book).State = EntityState.Modified;
+                    db.Entry(order).State = EntityState.Modified;    
+                } else if(order.State.Equals(returnState))
+                {
+                    Books book = db.Books.Find(order.BookID);
+                    Console.WriteLine("Titul: " + book.Title + " Status: " + book.IsAvailable + " Order state: " + order.State);
+                    book.IsAvailable = "Volná";
+                    order.State = "Vrácena";
+                    order.ReturnDate = DateTime.Now;
+                    Console.WriteLine("Titul: " + book.Title + " Status: " + book.IsAvailable + " Order state: " + order.State + " Date: " + order.ReturnDate);
+                    db.Entry(book).State = EntityState.Modified;
+                    db.Entry(order).State = EntityState.Modified;
+                }
+            }
+            db.SaveChanges();
+        }
+    }
+}
